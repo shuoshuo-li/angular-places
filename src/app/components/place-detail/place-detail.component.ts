534f6f4bdce327174c0fdf4d7771073867d2c74a
Shoushou Li <shoushou.li@estudiante.uam.es>
2025-03-05 12:50:54 +0100

    完成创建 Places 组件

---
+++ src/app/components/place-detail/place-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-place-detail',
+  imports: [],
+  templateUrl: './place-detail.component.html',
+  styleUrl: './place-detail.component.scss'
+})
+export class PlaceDetailComponent {
+
+}
---
+++ src/app/components/places-list/places-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-places-list',
+  imports: [],
+  templateUrl: './places-list.component.html',
+  styleUrl: './places-list.component.scss'
+})
+export class PlacesListComponent {
+
+}
